48852b2ebd19903a37a866341db9eaaaa6d66897
Max <steblevskiymaxim@ua.fm>
2023-02-01 05:44:57 +0200

    Update Calculator.jsx

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import OperandButton from "./OperandButton";
 import OperatorButton from "./OperatorButton";
 import {withStyles} from '@material-ui/core/styles';
-import {styled, TextField} from "@material-ui/core";
+import {List, ListItem, ListItemText, TextField} from "@material-ui/core";
 
 const styles = () => (
     {
@@ -24,10 +24,9 @@ const styles = () => (
 
 class Calculator extends React.Component {
     operators = ['+', '-', '*', "/"]
+    exerciseHistory = []
+    resultHistory = []
 
-    num1;
-    num2;
-    operator;
     constructor(props) {
         super(props);
         this.state = {
@@ -51,10 +50,10 @@ class Calculator extends React.Component {
     }
 
     deleteLastNumber() {
-        const input = this.state.input;
-        const inputLength = this.state.input.length;
+        let input = this.state.input;
+        let inputLength = input.length;
 
-        if (inputLength === 1 || input === 0) {
+        if (inputLength === 1 || !inputLength) {
             this.setState({
                 input: 0
             })
@@ -74,21 +73,65 @@ class Calculator extends React.Component {
         let prevInput = this.state.input;
         const lastSymbol = prevInput.toString().slice(-1);
 
-        if(this.operators.includes(lastSymbol)){
+        if (this.operators.includes(lastSymbol)) {
             this.setState({
                 input: this.state.input.toString().replace(lastSymbol, operator)
             })
         } else {
             this.setState({
-                input: this.state.input.toString() + operator})
+                input: this.state.input.toString() + operator
+            })
         }
+        if (operator === '=') {
+            const result = this.parseInput()
 
-        this.num1 = (this.num1) ? 0 : prevInput;
-        console.log(this.num1)
+            if (result) {
+                let num1 = result[1]
+                let num2 = result[3]
+                let operator = result[2]
+
+                this.calculate(num1, num2, operator)
+            }
+        }
     }
 
-    calculate(num1, num2, operator){
+    parseInput() {
+        const regExp = /(\d*)(.)(\d*)/;
+        let input = this.state.input;
+        let result = input.toString().match(regExp)
+        if (result[3] !== '') {
+            console.log(result)
+            return result
+        }
+    }
 
+    calculate(num1, num2, operator) {
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
+
+        let result
+
+        if (operator === '+') {
+            result = num1 + num2
+        } else if (operator === '-') {
+            result = num1 - num2
+        } else if (operator === '*') {
+            result = num1 * num2
+        } else if (operator === '/') {
+            result = num1 / num2
+        }
+
+        this.setState({
+            input: result.toString()
+        })
+
+        this.saveToHistory(num1, num2, operator, result)
+    }
+
+    saveToHistory(num1, num2, operator, result) {
+        let mathExercise = `${num1}${operator}${num2}=`
+        this.exerciseHistory.push(mathExercise)
+        this.resultHistory.push(result)
     }
 
 
@@ -123,6 +166,9 @@ class Calculator extends React.Component {
                     <OperatorButton operatorValue='/' onClick={() => this.enterOperator('/')}/>
                     <br/>
                     <br/>
+                    <OperatorButton operatorValue='=' onClick={() => this.enterOperator('=')}/>
+                    <br/>
+                    <br/>
                     <TextField
                         id="outlined-multiline-flexible"
                         multiline
@@ -132,7 +178,31 @@ class Calculator extends React.Component {
                         size='medium'
                     />
                 </div>
-                <h1>{this.state.input}</h1>
+                <div className="History">
+                    <List
+                        sx={{
+                            width: '100%',
+                            backgroundColor: 'background.paper',
+                            position: 'relative',
+                            overflow: 'auto',
+                            maxHeight: 300,
+                            '& ul': {padding: 0},
+                        }}
+                    >
+                        <h2 style={{
+                            fontFamily: 'Comic Sans MS',
+                            marginLeft: '110px'
+                        }}>History</h2>
+                        <ul>
+                            {this.exerciseHistory.map((item, index) => (
+                                <ListItem key={`${item}`}>
+                                    <ListItemText primary={`${item}${this.resultHistory[index]}`}/>
+                                </ListItem>
+
+                            ))}
+                        </ul>
+                    </List>
+                </div>
             </div>
         );
     }
