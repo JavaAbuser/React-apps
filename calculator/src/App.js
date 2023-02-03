4ff45b7e22032739a8f3bb8818c9bbb1b18f44dc
Max <steblevskiymaxim@ua.fm>
2023-02-03 04:54:58 +0200

    Update App.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Calculator from "./components/Calculator";
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import exercisesReducer from './pages/exercises/reduces/exercises';
+
+const store = createStore(exercisesReducer);
 
 class App extends React.Component{
 render(){
   return (
       <div className="App">
+          <Provider store={store}>
               <Calculator/>
+          </Provider>
       </div>
   );
 }
