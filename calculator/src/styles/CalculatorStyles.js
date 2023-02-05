43b9f359a861f6016c0bf63fe80ec6151694ce59
Max <steblevskiymaxim@ua.fm>
2023-02-05 04:43:22 +0200

    created CalculatorStyles

---
+++ calculator/src/styles/CalculatorStyles.js
@@ -0,0 +1,20 @@
+const Buttons = {
+    backgroundColor: '#94CDFF',
+    width: '15%',
+    height: '100%',
+    borderRadius: '15px 15px 15px 15px',
+    textAlign: 'center',
+    verticalAlign: 'center',
+    padding: '1px 20px 20px 20px'
+}
+
+const Header = {
+    fontFamily: 'Trebuchet MS',
+    fontSize: '30px',
+}
+
+const ButtonReceive = {
+    marginTop: '5px'
+}
+
+export {Buttons, Header, ButtonReceive}
